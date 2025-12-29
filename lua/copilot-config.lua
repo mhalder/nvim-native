ca22a01ec5efeaaf0d75217da12ce11935dd08e4
Martin Halder <halderm@arkadia-labs.io>
2025-12-29 15:33:38 +0100

    feat: add GitHub Copilot integration

---
+++ lua/copilot-config.lua
@@ -0,0 +1,23 @@
+require('copilot').setup({
+  suggestion = {
+    enabled = true,
+    auto_trigger = true,
+    keymap = {
+      accept = '<Tab>',
+      accept_word = '<C-Right>',
+      accept_line = '<C-Down>',
+      next = '<M-]>',
+      prev = '<M-[>',
+      dismiss = '<C-]>',
+    },
+  },
+  panel = {
+    enabled = true,
+  },
+  filetypes = {
+    yaml = true,
+    markdown = true,
+    gitcommit = false,
+    gitrebase = false,
+  },
+})
