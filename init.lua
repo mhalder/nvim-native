16e2c9f024295f909b80eeaca3232d84c6d4aa19
Martin Halder <halderm@arkadia-labs.io>
2025-12-30 23:44:19 +0100

    chore: sort plugin configs by priority and plugins alphabetically

--- init.lua
+++ init.lua
@@ -5,20 +5,20 @@ vim.g.mapleader = " "
 require("plugins")
 
 -- configure plugins
+require("treesitter-config")
 require("lsp-config")
 require("fidget-config")
-require("treesitter-config")
-require("blinkcmp-config")
-require("snacks-config")
-require("whichkey-config")
 require("luasnip-config")
+require("blinkcmp-config")
+require("copilot-config")
+require("trouble-config")
+require("conform-config")
+require("debugging-config")
+require("snacks-config")
 require("quicker-config")
 require("oil-config")
 require("sidekick-config")
-require("copilot-config")
-require("debugging-config")
-require("conform-config")
-require("trouble-config")
+require("whichkey-config")
 
 -- user setup
 require("options")
