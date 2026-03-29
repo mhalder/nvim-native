785e4d06063996adb2f0ac2b7c2be43288960c73
Martin Halder <halderm@arkadia-labs.io>
2026-03-29 13:42:41 +0200

    fix(dap): use extmark signs for breakpoints on nvim 0.12+
    nvim-dap uses legacy sign_place which no longer renders on 0.12+.
    Override toggle_breakpoint to place extmark signs with nerd font
    icons instead.

--- lua/debugging-config.lua
+++ lua/debugging-config.lua
@@ -5,6 +5,34 @@ require("dap-python").setup("uv")
 require("nvim-dap-virtual-text").setup({})
 dapui.setup()
 
+-- nvim-dap uses legacy sign_place which doesn't render on nvim 0.12+
+-- override toggle_breakpoint to place extmark signs instead
+local bp_ns = vim.api.nvim_create_namespace("dap_breakpoint_signs")
+local bp_signs = {
+  DapBreakpoint = { text = "\u{f28d}", hl = "DiagnosticError" },
+  DapBreakpointCondition = { text = "\u{f059}", hl = "DiagnosticWarn" },
+  DapStopped = { text = "\u{f04b}", hl = "DiagnosticOk", linehl = "DiffAdd" },
+}
+
+local orig_toggle = dap.toggle_breakpoint
+dap.toggle_breakpoint = function(...)
+  local bufnr = vim.api.nvim_get_current_buf()
+  local lnum = vim.api.nvim_win_get_cursor(0)[1]
+  local marks = vim.api.nvim_buf_get_extmarks(bufnr, bp_ns, { lnum - 1, 0 }, { lnum - 1, 0 }, {})
+  if #marks > 0 then
+    for _, mark in ipairs(marks) do
+      vim.api.nvim_buf_del_extmark(bufnr, bp_ns, mark[1])
+    end
+  else
+    local sign = bp_signs.DapBreakpoint
+    vim.api.nvim_buf_set_extmark(bufnr, bp_ns, lnum - 1, 0, {
+      sign_text = sign.text,
+      sign_hl_group = sign.hl,
+    })
+  end
+  return orig_toggle(...)
+end
+
 -- Rust debugging with codelldb
 local codelldb_path = vim.fn.stdpath("data") .. "/mason/packages/codelldb/extension/adapter/codelldb"
 
